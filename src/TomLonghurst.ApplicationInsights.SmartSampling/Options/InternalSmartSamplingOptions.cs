56df15a5c3bcfb66236e5ea2698907ea2190aa3a
Tom Longhurst <tom.longhurst@asos.com>
2021-12-20 12:13:19 +0000

    Redundant initialisers

--- src/TomLonghurst.ApplicationInsights.SmartSampling/Options/InternalSmartSamplingOptions.cs
+++ src/TomLonghurst.ApplicationInsights.SmartSampling/Options/InternalSmartSamplingOptions.cs
@@ -6,21 +6,21 @@ namespace TomLonghurst.ApplicationInsights.SmartSampling.Options;
 
 internal class InternalSmartSamplingOptions
 {
-    public ImmutableArray<Func<ITelemetry, bool>> AnyTelemetryTypeDoNotSampleEntireJourneyRules { get; set; } = new();
+    public ImmutableArray<Func<ITelemetry, bool>> AnyTelemetryTypeDoNotSampleEntireJourneyRules { get; set; }
     
-    public ImmutableArray<Func<DependencyTelemetry, bool>> DependencyDoNotSampleEntireJourneyRules { get; set; } = new();
+    public ImmutableArray<Func<DependencyTelemetry, bool>> DependencyDoNotSampleEntireJourneyRules { get; set; }
 
-    public ImmutableArray<Func<RequestTelemetry, bool>> RequestDoNotSampleEntireJourneyRules { get; set; } = new();
+    public ImmutableArray<Func<RequestTelemetry, bool>> RequestDoNotSampleEntireJourneyRules { get; set; }
       
-    public ImmutableArray<Func<EventTelemetry, bool>> CustomEventDoNotSampleEntireJourneyRules { get; set; } = new();
+    public ImmutableArray<Func<EventTelemetry, bool>> CustomEventDoNotSampleEntireJourneyRules { get; set; }
       
-    public ImmutableArray<Func<TraceTelemetry, bool>> TraceDoNotSampleEntireJourneyRules { get; set; } = new();
+    public ImmutableArray<Func<TraceTelemetry, bool>> TraceDoNotSampleEntireJourneyRules { get; set; }
       
-    public ImmutableArray<Func<ExceptionTelemetry, bool>> ExceptionDoNotSampleEntireJourneyRules { get; set; } = new();
+    public ImmutableArray<Func<ExceptionTelemetry, bool>> ExceptionDoNotSampleEntireJourneyRules { get; set; }
     
-    public ImmutableArray<Func<PageViewPerformanceTelemetry, bool>> PageViewPerformanceDoNotSampleEntireJourneyRules { get; set; } = new();
+    public ImmutableArray<Func<PageViewPerformanceTelemetry, bool>> PageViewPerformanceDoNotSampleEntireJourneyRules { get; set; }
     
-    public ImmutableArray<Func<PageViewTelemetry, bool>> PageViewDoNotSampleEntireJourneyRules { get; set; } = new();
+    public ImmutableArray<Func<PageViewTelemetry, bool>> PageViewDoNotSampleEntireJourneyRules { get; set; }
     
     public TimeSpan SendTelemetryNotLinkedToRequestsAfter { get; set; } = TimeSpan.FromMinutes(2);
 }
